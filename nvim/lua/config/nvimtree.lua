805f9205f59897d25a28a0b6067db8438c980971
Anderson Sequera <andersonsp24@gmail.com>
2022-09-06 15:18:36 +0100

    Initial commit

---
+++ nvim/lua/config/nvimtree.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+function M.setup()
+  require("nvim-tree").setup {
+    disable_netrw = true,
+    hijack_netrw = true,
+		respect_buf_cwd = true,
+    view = {
+      number = true,
+      relativenumber = true,
+    },
+    filters = {
+      custom = { ".git" },
+    },
+    update_cwd = true,
+    update_focused_file = {
+      enable = true,
+      update_cwd = true,
+    },
+  }
+end
+
+return M
+
